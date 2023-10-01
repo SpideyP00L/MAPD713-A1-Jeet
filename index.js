58bbf75cf15f51b19a6459bf79583de27a2fc596
Jeet Panchal <jeetbpanchal@gmail.com>
2023-09-30 23:42:38 -0400

    Your commit message here

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 let SERVER_NAME = 'Assignment 1'
 let PORT = 5000;
-let HOST = '127.0.0.1:5000';
+let HOST = '127.0.0.1';
 let errors = require('restify-errors');
 let restify = require('restify')
 
   // Get a persistence engine for the products
-  , usersSave = require('save')('products')
+  , productsSave = require('save')('products')
 
   // Create the restify server
-  , server = restify.createServer({ name: SERVER_NAME})
+let server = restify.createServer({ name: SERVER_NAME });
 
-  server.listen(PORT, HOST, function () {
-  console.log('Server : %s listening at : %s', server.name, server.url)
-  console.log('Endpoints : %s/products method : GET and POST', server.url)  
-})
+let getRequestCounter = 0; // Counter for GET requests
+let postRequestCounter = 0; // Counter for POST requests
+
+server.listen(PORT, HOST, function () {
+  console.log('Server %s listening at %s', server.name, server.url);
+  console.log('Endpoints %s/products Method : GET, POST and DELETE',server.url)
+});
 
 server.use(restify.plugins.fullResponse());
 server.use(restify.plugins.bodyParser());
+
+// Middleware to count GET and POST requests and log their values
+server.use(function (req, res, next) {
+  if (req.method === 'GET') {
+    getRequestCounter++;
+  } else if (req.method === 'POST') {
+    postRequestCounter++;
+  }
+  console.log(`Processed Request Count--> Get:${getRequestCounter}, Post:${postRequestCounter}`);
+  next();
+});
 
 // Get all products in the system
 server.get('/products', function (req, res, next) {
@@ -24,19 +38,17 @@ server.get('/products', function (req, res, next) {
 
   // Find every entity within the given collection
   productsSave.find({}, function (error, products) {
-
-    // Return all of the users in the system
-    res.send(products)
-  })
-})
+    // Return all of the products in the system
+    res.send(products);
+  });
+});
 
 // Get a single product by their user id
 server.get('/products/:id', function (req, res, next) {
   console.log('GET /products/:id params=>' + JSON.stringify(req.params));
 
   // Find a single product by their id within save
-  usersSave.findOne({ _id: req.params.id }, function (error, user) {
-
+  productsSave.findOne({ _id: req.params.id }, function (error, user) {
     // If there are any errors, pass them to next in the correct format
     if (error) return next(new Error(JSON.stringify(error.errors)))
 
@@ -47,15 +59,15 @@ server.get('/products/:id', function (req, res, next) {
       // Send 404 header if the user doesn't exist
       res.send(404)
     }
-  })
-})
+  });
+});
 
-// Create a new user
+// Create a new Product
 server.post('/products', function (req, res, next) {
   console.log('POST /products params=>' + JSON.stringify(req.params));
-  console.log('POST /productss body=>' + JSON.stringify(req.body));
+  console.log('POST /products body=>' + JSON.stringify(req.body));
 
-  // validation of manadatory fields
+  // validation of mandatory fields
   if (req.body.pId === undefined ) {
     // If there are any errors, pass them to next in the correct format
     return next(new errors.BadRequestError('Product ID is mandatory'))
@@ -75,6 +87,7 @@ server.post('/products', function (req, res, next) {
     // If there are any errors, pass them to next in the correct format
     return next(new errors.BadRequestError('Product Quantity is mandatory'))
   }
+
   let newProduct = {
 		pId: req.body.pId, 
 		pname: req.body.pname,
@@ -84,66 +97,26 @@ server.post('/products', function (req, res, next) {
 
   // Create the product using the persistence engine
   productsSave.create( newProduct, function (error, product) {
-
     // If there are any errors, pass them to next in the correct format
     if (error) return next(new Error(JSON.stringify(error.errors)))
 
     // Send the product if no issues
     res.send(201, product)
-  })
-})
+  });
+});
 
-// Update a product by their id
-server.put('/products/:id', function (req, res, next) {
-  console.log('POST /products params=>' + JSON.stringify(req.params));
-  console.log('POST /products body=>' + JSON.stringify(req.body));
-  // validation of manadatory fields
-  if (req.body.pId === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(new errors.BadRequestError('Product ID is Mandatory'))
-  }
-  if (req.body.pname === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(new errors.BadRequestError('Product Name is Mandatory'))
-  }
-  if (req.body.pprice === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(new errors.BadRequestError('Product Price is Mandatory'))
-  }
-  if (req.body.pquantity === undefined ) {
-    // If there are any errors, pass them to next in the correct format
-    return next(new errors.BadRequestError('Product Quantity is Mandatory'))
-  }
+// Delete all products
+server.del('/products', function (req, res, next) {
+  console.log('DELETE /products');
 
-  let newProduct = {
-		pId: req.body.pId,
-		pname: req.body.pname, 
-		pprice: req.body.pprice,
-    pquantity: req.body.pquantity
-	}
-  
-  // Update the Product with the persistence engine
-  productsSave.update(newProduct, function (error, product) {
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new Error(JSON.stringify(error.errors)))
+  // Delete all products in the persistence engine
+  productsSave.deleteMany({}, function (error) {
+    if (error) {
+      // If there are any errors, pass them to next in the correct format
+      return next(new Error(JSON.stringify(error.errors)));
+    }
 
-    // Send a 200 OK response
-    res.send(200)
-  })
-})
-
-// Delete product with the given id
-server.del('/products/:id', function (req, res, next) {
-  console.log('POST /products params=>' + JSON.stringify(req.params));
-  // Delete the Product with the persistence engine
-  productsSave.delete(req.params.id, function (error, product) {
-
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new Error(JSON.stringify(error.errors)))
-
-    // Send a 204 response
-    res.send(204)
-  })
-})
-
-
+    // Send a 204 No Content response to indicate that all products were deleted
+    res.send(204);
+  });
+});
